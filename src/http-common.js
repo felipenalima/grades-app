28fbde0670c0c127d247a20ada524d83e1902ef6
felipe.lima <felipena.lima@gmail.com>
2020-08-31 12:57:27 -0300

    fix: fixed environment bugs

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://gradesapi-challenge.herokuapp.com',
+  baseURL: 'https://gradesapi-challenge.herokuapp.com/grade',
   headers: {
     'Content-type': 'application/json',
   },
